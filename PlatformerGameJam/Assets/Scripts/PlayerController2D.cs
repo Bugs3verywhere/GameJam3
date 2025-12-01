b5c6868635db107c5f194db4681db0da36303435
Quillir <philweav@outlook.com>
2025-12-01 17:02:53 +0000

    Imported map, modified character controller
    Imported the map I'd been making and added a sprite atlas to removing tearing of the sprites when viewed in-game.
    Modified the character controller script to work with 2D colliders (the old script is still intact if needed).

---
+++ PlatformerGameJam/Assets/Scripts/PlayerController2D.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+
+    //Declaring basic variables
+    public Rigidbody2D player;
+    public SpriteRenderer playerSprite;
+    public bool touchingGround = false;
+    public float facingDirection;
+    public Camera mainCam;
+
+    //Movement speed values
+    public float walkSpeed = 2f;
+    public float sprintSpeed = 4f; //Not implemented yet
+    public float jumpSpeed = 5f;
+    public float cameraDistance = 10f;
+
+    void Update()
+    {
+        //Camera follows player
+        mainCam.transform.position = new Vector3(player.position.x, player.position.y + 1, -cameraDistance);
+
+        //Player movement
+        float facingDirection = 0f;
+
+        if (Input.GetKey(KeyCode.A) & !Input.GetKey(KeyCode.D))
+        {
+            facingDirection = -1;
+            playerSprite.flipX = true;
+        }
+
+        else if (Input.GetKey(KeyCode.D) & !Input.GetKey(KeyCode.A))
+        {
+            facingDirection = 1;
+            playerSprite.flipX = false;
+        }
+
+        player.velocity = new Vector3(walkSpeed * facingDirection, player.velocity.y, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space) && touchingGround)
+        {
+            player.velocity = new Vector3(player.velocity.x, jumpSpeed, 0);
+            touchingGround = false;
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Check floor collision
+        if (collision.collider.CompareTag("Floor"))
+        {
+            touchingGround = true;
+        }
+    }
+}
